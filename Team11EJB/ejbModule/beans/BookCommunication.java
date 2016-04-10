05e3741bdb8378de6a9e473598758eaadd23354e
Tatjana Sajtos <fenix1215@gmail.com>
2016-04-10 21:42:17 +0200

    Dodati bean-ovi za knjigu i korisnika

---
+++ Team11EJB/ejbModule/beans/BookCommunication.java
@@ -0,0 +1,76 @@
+package beans;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import model.Book;
+import model.Category;
+import model.Impression;
+import model.User;
+
+/**
+ * Session Bean implementation class BookCommunication
+ */
+@Stateless
+@LocalBean
+public class BookCommunication implements BookCommunicationRemote {
+
+	@PersistenceContext
+	EntityManager em;
+	
+	
+    public BookCommunication() {
+        // TODO Auto-generated constructor stub
+    }
+    
+       
+    public boolean addBook(String author, String title, String description, String picture, User userID) {
+    	TypedQuery<Book> q = em.createQuery("SELECT b FROM Book b WHERE :title like b.title and :author like b.author", Book.class);
+    	q.setParameter("title", title);
+    	q.setParameter("author", author);
+    	Book test = q.getSingleResult();
+    	
+    	if (test != null) {
+    		//TODO: MESSAGE TO USER, BOOK EXISTS!
+    		return false;
+    	}
+    	
+    	Book newBook = new Book();
+    	newBook.setAuthor(author);
+    	newBook.setDescription(description);
+    	newBook.setTitle(title);
+    	newBook.setPicture(picture);
+    	newBook.setUser(userID);
+    	
+    	em.persist(newBook);
+    	return true;
+    }
+    
+    public boolean addImpressions(Impression impression, Book book) {
+    	TypedQuery<Book> q = em.createQuery("SELECT b FROM Book b WHERE :title like b.title and :author like b.author", Book.class);
+    	q.setParameter("title", book.getTitle());
+    	q.setParameter("author", book.getAuthor());
+    	Book test = q.getSingleResult();
+    	
+    	if (test == null) {
+    		//TODO: MESSAGE TO USER, BOOK DOES'T EXISTS!
+    		return false;
+    	}
+    	
+    	List<Impression> list = book.getImpressions();
+    	list.add(impression);
+    	book.setImpressions(list);
+    	impression.setBook(book);
+    	
+    	em.persist(impression);
+    	em.merge(book);
+    	
+    	return true;
+    }
+
+}
---
+++ Team11EJB/ejbModule/beans/CommunicationWithInterface.java
@@ -0,0 +1,84 @@
+package beans;
+
+import java.util.Date;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import model.Book;
+import model.Impression;
+import model.User;
+
+/**
+ * Session Bean implementation class CommunicationWithInterface
+ */
+@Stateful
+public class CommunicationWithInterface implements CommunicationWithInterfaceRemote {
+
+	@PersistenceContext
+	EntityManager em;
+	
+	@EJB
+	BookCommunication bookComm;
+	
+	private User userID;
+	
+    public CommunicationWithInterface() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public boolean login(String username, String password){
+    	TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE :username like u.username and u.password like :password", User.class);
+    	query.setParameter("username", username);
+    	query.setParameter("password", password);
+    	User user = query.getSingleResult();
+    	if(user != null){
+    		userID = user;
+    	
+    		return true;
+    	}
+    	return false;
+    }
+    
+    
+    public boolean register(String username, String password, String name, String lastName, String email, String phoneNumber, Date birthDate) {
+    	TypedQuery<User> query = em.createQuery("SELECT u FROM User u WHERE :username like u.username", User.class);
+    	query.setParameter("username", username);
+    	User test = query.getSingleResult();
+    	if (test != null) {
+    		//TODO: Message to user
+    		return false;
+    	}
+    	
+    	User newUser = new User();
+		newUser.setUsername(username);
+		newUser.setPassword(password);
+		newUser.setName(name);
+		newUser.setLastName(lastName);
+		newUser.setEmail(email);
+		newUser.setPhoneNumber(phoneNumber);
+		newUser.setBirthDate(birthDate);
+		
+		em.persist(newUser);
+		return true;
+    	
+    }
+    
+    public boolean addBook(String author, String title, String description, String picture) {
+    		
+    	return bookComm.addBook(author, title,description,picture,userID);
+    }
+    
+    public boolean addImpression(String text, Date date, Book book ) {
+    	Impression impression = new Impression();
+    	impression.setText(text);
+    	impression.setDate(date);
+    	impression.setUser(userID);
+    	
+    	return bookComm.addImpressions(impression, book);
+    }
+
+}
